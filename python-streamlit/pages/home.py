4ff9124c6e747e80073afe5358aa59b806eb12c4
Majd <majdbitar2003@gmail.com>
2024-08-11 00:23:13 +0300

    Finished streamlit script to visualize predicted data

--- python-streamlit/pages/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def home_page():
-    st.title("Welcome to the Home Section")
-    st.write("This is the Home section.")
--- python-streamlit/pages/our_story.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def our_story_page():
-    st.title("Our Story")
-    st.write("This is the Our Story section.")
